d077ff7cfc33b714a5656c1bf8797be4b865303e
Elvan Ekilsin <78863009+eelvoon@users.noreply.github.com>
2021-02-10 18:13:39 +0300

    Add files via upload
    iOMANİP KÜTÜPHANESİNE ÖRNEK UYGULAMA :)))

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+//setfill boþ kalan yere ekler. setw ise karakter sayýsýný söylersin.
+
+
+int a=117;
+cout<<setfill('*')<<setw(10)<<a<<endl; 
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
